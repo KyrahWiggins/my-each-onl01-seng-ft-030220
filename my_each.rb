3fcf83f0f0a55d2fdc2cfd1d35c6a323619e3442
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-11 00:48:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,15 +3,11 @@ list = ['arel', 'jon', 'logan', 'spencer']
 def my_each(array)
   i = 0
   while i < array.length
-# The "while" method will always return a value of => nil"
     yield(array[i])
     i = i + 1
   end
+my_each(list)
 end
-
-# Use the collection method
-# To make the *return-value* the answer you want
-# instead of "=> nil"
 
 def hello(array)
   i = 0
