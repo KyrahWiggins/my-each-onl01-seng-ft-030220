65867a03493cfff11215f64e948ea291cf46dc64
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-11 01:28:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,18 +3,7 @@ def my_each(array)
   i = 0
   while i < array.length
     yield(array[i])
-    i = i + 1
-end
-puts my_each(list)
-end
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
     i += 1
   end
-  collection
+  array
 end
-
-hello(list) {|i| "Hello #{i} what's up?"}
